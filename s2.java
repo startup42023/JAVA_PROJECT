7467abaecd9738baca5b1b98f45072634598bc03
admin <startup42023@gmail.com>
2023-02-28 10:03:00 +0530

    Initial commit

---
+++ s2.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        method1();
+    }
+
+    public static void method1() {
+        System.out.println("Hello Method 1");
+    }
+}
